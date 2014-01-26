c1f19126ccc8e53e576fe3de7e36c1d60f16a8b4
Sairiz <saiffil@mtouch.my>
2014-01-27 00:08:59 +0800

    Edit larafuse route test

--- src/routes.php
+++ src/routes.php
@@ -59,7 +59,7 @@ Route::group(array('prefix' => 'larafuse'), function()
 {
     Route::get('/test', function()
     {
-        return Fuse::loadCon(17,['FirstName']);
+        return Fuse::dsQuery('Product',1,0,['Id' -> '%'],['Id','ProductName']);
     });
 
 	Route::get('fetch/{table?}', 'Saiffil\Larafuse\Controllers\LarafuseController@fetch');
