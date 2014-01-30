5ad8c841e8b16a9722250d7f47dfec582d63e7b9
Sairiz <saiffil@mtouch.my>
2014-01-30 10:08:52 +0800

    Fix fetchtable refresh url and timer

--- src/views/larafuse/fetchtable.blade.php
+++ src/views/larafuse/fetchtable.blade.php
@@ -2,7 +2,7 @@
 <head>
 <?php 
 	if($nextPage != null) { ?>
-		<meta http-equiv="refresh" content="{{ '15' }}; url=/larafuse/fetchTable/{{ $table }}/{{ $nextPage }}" />
+		<meta http-equiv="refresh" content="{{ '10' }}; url=/larafuse/fetchtable/{{ $table }}/{{ $nextPage }}" />
 <?php } ?>
 
 
